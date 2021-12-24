03fcc60fd37fdc0c1261c02714ce94eac123e90f
dobin_shin <dobin_shin@tmax.co.kr>
2021-12-24 11:25:00 +0900

    first init

---
+++ App.js
@@ -0,0 +1,86 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import SerialPortAPI from 'react-native-serial-port-api';
+
+async function example() {
+  const serialPort = await SerialPortAPI.open('/dev/ttyS4', {baudRate: 38400});
+
+  // subscribe received data
+  const sub = serialPort.onReceived(buff => {
+    console.log(buff.toString('hex').toUpperCase());
+  });
+
+  // unsubscribe
+  // sub.remove();
+
+  // send data with hex format
+  await serialPort.send('00FF');
+
+  // close
+  serialPort.close();
+}
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Text>Serial Port Test</Text>
+          <Button onPress={() => example()} title="serialPort call" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
